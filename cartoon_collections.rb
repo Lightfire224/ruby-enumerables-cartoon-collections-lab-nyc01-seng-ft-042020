60d019a742945ed885e13a89abb89fb9fd16d639
Elias Taveras <eliastave12@gmail.com>
2020-03-26 19:41:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,8 @@ def long_planeteer_calls(calls_long)
   false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
